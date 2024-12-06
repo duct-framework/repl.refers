b59da8f0996b79b578855a378be299b7cfe30e5b
James Reeves <jreeves@weavejester.com>
2024-12-06 01:44:29 +0000

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.duct-framework/repl.refers "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Integrant methods to add vars to user namespace"
+  :url "https://github.com/duct-framework/repl.refers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.0"]
