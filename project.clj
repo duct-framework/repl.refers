744ef4a7070e038bbbf7df3384402d8bd2ff141c
James Reeves <jreeves@weavejester.com>
2025-10-08 14:07:20 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/duct-framework/repl.refers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [integrant/integrant "0.13.1"]])
+  :dependencies [[org.clojure/clojure "1.12.3"]
+                 [integrant/integrant "1.0.1"]])
